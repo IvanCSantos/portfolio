05f93859fd260bd845a9d2bd19f8303a49a4db0c
Ivan Santos <ivancsantos@hotmail.com.br>
2025-05-22 20:00:04 -0300

    preparando a estrutura inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./App.css";
+import { Header } from "./components/Header/Header.tsx";
+import { Routes, Route, useNavigate } from "react-router";
+import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import { Career } from "./pages/Carreer/Career";
+import { Curriculum } from "./pages/Curriculum/Curriculum";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/curriculum" element={<Curriculum />} />
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <div>LOGO</div>
+      <div>
+        <ul>
+          <li>Sobre</li>
+          <li>Carreira</li>
+          <li>Projetos</li>
+          <li>Curriculo</li>
+        </ul>
+      </div>
+      <div>IDIOMA</div>
+    </header>
+  );
+};
